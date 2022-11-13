bcb075ff6452d8e897563528aec5bf530976f69e
oleg <music.wave.form7@gmail.com>
2022-11-13 14:09:14 +0100

    fix export issue

--- aux_math.js
+++ aux_math.js
@@ -1,1 +1,7 @@
-var addon = require('bindings')('aux_math.node')
+let addon = require('bindings')('aux_math.node')
+
+module.exports = {
+    aux_add: addon.aux_add,
+    aux_sub: addon.aux_sub,
+    aux_multi: addon.aux_multi,
+};
